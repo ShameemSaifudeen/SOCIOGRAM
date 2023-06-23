954403726bfb8e20dbebf14419f18396711055f5
ShameemSaifudeen <shameemsaif456@gmail.com>
2023-06-23 11:20:09 +0530

    error

--- client/src/pages/Call/Call.jsx
+++ client/src/pages/Call/Call.jsx
@@ -1,63 +1,43 @@
 import { useParams } from "react-router-dom";
 import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
-import Navbar from "../NavBar/NavBar";
+import Navbar from "../NavBar/NavBar"
 import { Box } from "@mui/material";
-import { useRef, useEffect } from "react";
-
-// import configKeys from "../../config";
-
-function randomID(len) {
-  let result = "";
-  if (result) return result;
-  var chars = "12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP",
-    maxPos = chars.length,
-    i;
-  len = len || 5;
-  for (i = 0; i < len; i++) {
-    result += chars.charAt(Math.floor(Math.random() * maxPos));
-  }
-  return result;
-}
+import configKeys from "../../config";
 
 const CallPage = () => {
   const { roomId } = useParams();
-  const myMeetingRef = useRef(null);
 
-  useEffect(() => {
-    const startMeeting = async (element) => {
-      const appID = 36793106;
-      const serverSecret = "42d9712ed63d054114625f34dfc55404";
-      const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
-        appID,
-        serverSecret,
-        roomId,
-        randomID(5),
-        randomID(5)
-        // "Enter NickName"
-      );
-      const zc = ZegoUIKitPrebuilt.create(kitToken);
-      zc.joinRoom({
+  const myMeeting = async (element) => {
+    const appID = 36793106;
+    const serverSecret = "42d9712ed63d054114625f34dfc55404";
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomId,
+      Date.now().toString(),
+      "Enter NickName"
+    );
+   console.log(kitToken,":::::::");
+    const zc = ZegoUIKitPrebuilt.create(kitToken)
+    console.log(zc,"::::::::")
+    zc.joinRoom({
         container: element,
         sharedLinks: [
-          {
-            name: "Copy Link",
-            url: `https://sociograam.online/room/${roomId}`,
-          },
+            {
+                name:"Copy Link",
+                url: `https://sociograam.online/room/${roomId}`
+            }
         ],
-        scenario: {
-          mode: ZegoUIKitPrebuilt.OneONoneCall,
+        scenario:{
+            mode: ZegoUIKitPrebuilt.OneONoneCall
         },
-        showScreenSharingButton: false,
-      });
-    };
-
-    startMeeting(myMeetingRef.current);
-  }, [roomId]);
-
+        showScreenSharingButton:false
+    })
+  };
   return (
     <div>
-      <Navbar />
-      <div style={{ padding: "7rem" }} ref={myMeetingRef} />
+      <Navbar/>
+      <div style={{padding: "7rem"}} ref={myMeeting}/>
     </div>
   );
 };
