4cae9bb407564f89bd8855cc34f1800feb1b6c1a
ShameemSaifudeen <shameemsaif456@gmail.com>
2023-07-15 00:23:01 +0530

    comments bug fixed

--- client/src/components/AdminTable/PostTable.jsx
+++ client/src/components/AdminTable/PostTable.jsx
@@ -30,7 +30,6 @@ const PostTable = () => {
   const [page, setPage] = useState(1);
   const [rowsPerPage] = useState(10);
   const [filterReported, setFilterReported] = useState(false); 
-  console.log("hhhhhhhhhhhhhhh");
 
   const handleDelete = async (postId) => {
     setSelectedPostId(postId);
